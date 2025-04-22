f864a22e9b99db2f2a79474dd26c3e2efd93f87e
Jean Pool Zambrano <jean_pool.98@hotmail.com>
2025-04-22 18:52:37 -0500

    Se actualiza controlador hello

--- microservice-hello/src/main/java/com/sabana/taller/microservice_hello/HelloController.java
+++ microservice-hello/src/main/java/com/sabana/taller/microservice_hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "¡Hola mundo desde una nueva versión!";
+        return "¡Hola mundo desde una nueva versión 2!";
     }
 }
